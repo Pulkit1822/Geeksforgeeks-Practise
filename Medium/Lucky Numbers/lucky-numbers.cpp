b12ea407f5ad72eba202e3259a31f90ca7f0a8d5
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2024-02-09 10:39:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Lucky Numbers/lucky-numbers.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+    bool check(int n,int counter)
+    {
+        if(counter<=n) {
+            if(n%counter==0)
+                return false;
+            n = n-n/counter;
+            counter++;
+            return check(n, counter);
+        }
+        else
+            return true;
+    }
+    bool isLucky(int n) {
+        return check(n, 2);
+    }
+};
+
+//{ Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}
+// } Driver Code Ends
