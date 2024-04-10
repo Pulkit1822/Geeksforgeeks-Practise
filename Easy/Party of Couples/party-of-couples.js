4756877083c4a344d33868f8baade765c50faf29
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2024-04-10 13:14:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.js
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.findSingle(n, arr);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for javascript
+
+
+/**
+ * @param {number} n
+ * @param {number[]} arr
+ * @returns {number}
+*/
+
+class Solution{
+    findSingle(n, arr){
+        //code here
+        var xor = 0;
+        for(var i in arr)
+        xor = xor ^ arr[i];
+        return xor;
+    }
+}
