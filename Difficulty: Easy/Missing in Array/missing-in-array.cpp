96c464474303436d49d900eb117f77082ea352ab
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2024-09-05 00:27:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int missingNumber(int n, vector<int>& arr) {
+  long sum = accumulate(arr.begin(), arr.end(), 0l);
+        long total= (n*1l)*(n + 1)/2;
+        return total- sum;
+        // Your code goes here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n - 1);
+        for (int i = 0; i < n - 1; ++i)
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.missingNumber(n, arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
